4bbc3d3c41072a8cb551272a4e0d07ab48e51dae
rhmiller47 <rmiller47@mac.com>
2014-06-13 18:23:56 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,10 @@
 ## Write a short comment describing this function
 
 ## makeCacheMatrix involves several commands used to set, get, 
-## and solve for the inversion of a Matrix
+## and solve for the inversion of a Matrix and will be used in 
+## cacheSolve funtion listed below. The <<- operator is used 
+## to assign a value to an object in an environment 
+## that is different from the current environment. 
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -26,18 +29,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-## Solve for the inverse of a matrix, if the matrix is the same, 
-## use cached inverse matrix
+## The following function calculates the inverse of a matrix created using 
+## makeCacheMatrix (above). The function, cacheSolve, will firts checks to 
+## see if an inverse of the matrix has already been calculated. If so, it 
+## gets the inverse from the cache value and skips the computation.  
 
 cacheSolve <- function(x = matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
     m <- x$getsolve()
+    ## if inverse exist skip computation and return
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
     data <- x$get()
     m <- solve(data, ...)
+    ## Set the inverse value
     x$setsolve(m)
     m
 }
