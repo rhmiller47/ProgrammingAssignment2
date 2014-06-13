8ae9ad33781b4f8207d482d854b1b922078b4537
rhmiller47 <rmiller47@mac.com>
2014-06-13 18:12:55 -0400

    Update cachematrix.R
    6.13.2014: Inserted new functional commands to process matrix and to create inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,44 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix involves several commands used to set, get, 
+## and solve for the inversion of a Matrix
 
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    ## set the matrix arguements
+    set <- function(y=matrix()) {
+        x <<- y
+        m <<- NULL
+    }
+    ## get matrix arguements 
+    get <- function() x
+    ## functions used in cacheSolve 
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    ## list of commands creaated by makeCacheMatrix
+    list(set = set, get = get, setsolve = setsolve,
+                getsolve = getsolve)  
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+## Solve for the inverse of a matrix, if the matrix is the same, 
+## use cached inverse matrix
+
+cacheSolve <- function(x = matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
+
+}
+
